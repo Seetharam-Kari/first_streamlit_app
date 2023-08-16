37b7917cbbabe22b89075a5539384826c08c50a6
Seetharam-Kari <142438650+Seetharam-Kari@users.noreply.github.com>
2023-08-16 23:43:09 +0530

    Update streamlit_app.py
    Adding more test content

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,6 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
 
-streamlit.write("Omega")
+streamlit.write("Omega 3, 4 Blueberry Oatmeal")
+streamlit.write("Kale,Spinach and Rocket Smotthies")
+streamlit.write("Hard boiled free range egg")
