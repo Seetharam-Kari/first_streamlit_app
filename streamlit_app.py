17d5f6e2f01074bb3e93e9f91674cc5393b696ca
Seetharam-Kari <142438650+Seetharam-Kari@users.noreply.github.com>
2023-08-24 13:30:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,3 @@ import snowflake.connector
 
 
 
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
