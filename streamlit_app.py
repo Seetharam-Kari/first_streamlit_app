d1af505cff5ca98c6a23458cae2bca94f5ab19be
Seetharam-Kari <142438650+Seetharam-Kari@users.noreply.github.com>
2023-08-24 13:37:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,11 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+
+my_cur.execute("select * from fruit_load_list")
+
+my_data_row = my_cur.fetchall()
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
