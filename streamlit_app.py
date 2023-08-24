538f100e2907d3b4e57209160ae684a949f47e69
Seetharam-Kari <142438650+Seetharam-Kari@users.noreply.github.com>
2023-08-24 13:43:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,4 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 
 my_data_row = my_cur.fetchall()
-
 my_cur.execute("insert into fruit_load_list values('from streamlit')")
